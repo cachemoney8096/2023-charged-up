02a644237dba9204d987e19884499db4a99d9e4e
avakimc <acohen1@ucls.uchicago.edu>
2023-02-03 20:07:07 -0600

    Finished Light Subsystem
    - made toggle methods for all lightCodes including OFF
    - made setLight method that changes the light to the one passed in
    - made setColor method that is used in setLight and toggle methods to change the RGB of the led
    - made setSpacing method that sets the spacing between leds in the strip so not all are lit up at once (default is all are lit at once)

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -1,12 +1,20 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap;
 
-// TODO do this subsystem
 public class Lights extends SubsystemBase {
   private LightCodes currentLightStatus = LightCodes.OFF;
+  private AddressableLED m_led = new AddressableLED(RobotMap.LED_PWM_PORT);
+  private AddressableLEDBuffer m_ledBuffer;
+  private int spacing = 1;
 
-  public Lights() {}
+  public Lights() {
+    m_ledBuffer = new AddressableLEDBuffer(60);
+    m_led.setLength(m_ledBuffer.getLength());
+  }
 
   public enum LightCodes {
     CONE, // Solid Yellow
@@ -24,6 +32,9 @@ public class Lights extends SubsystemBase {
     } else {
       currentLightStatus = LightCodes.CONE;
     }
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
   }
 
   public void toggleCubeLight() {
@@ -31,6 +42,98 @@ public class Lights extends SubsystemBase {
       currentLightStatus = LightCodes.OFF;
     } else {
       currentLightStatus = LightCodes.CUBE;
+    }
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  public void toggleGameObjectLight() {
+    if (currentLightStatus == LightCodes.GAME_OBJECT) {
+      currentLightStatus = LightCodes.OFF;
+    } else {
+      currentLightStatus = LightCodes.GAME_OBJECT;
+    }
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  public void toggelNoTagLight() {
+    if (currentLightStatus == LightCodes.NO_TAG) {
+      currentLightStatus = LightCodes.OFF;
+    } else {
+      currentLightStatus = LightCodes.NO_TAG;
+    }
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  public void toggleWorkingLight() {
+    if (currentLightStatus == LightCodes.WORKING) {
+      currentLightStatus = LightCodes.OFF;
+    } else {
+      currentLightStatus = LightCodes.WORKING;
+    }
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  public void toggleReadyToScoreLight() {
+    if (currentLightStatus == LightCodes.READY_TO_SCORE) {
+      currentLightStatus = LightCodes.OFF;
+    } else {
+      currentLightStatus = LightCodes.READY_TO_SCORE;
+    }
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  public void toggleOff() {
+    currentLightStatus = LightCodes.OFF;
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  public void setLight(LightCodes light) {
+    currentLightStatus = light;
+    setColor(currentLightStatus);
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
+
+  /** Sets the spacing between each LED so not all are lit up when applicable */
+  public void setSpacing(int s) {
+    spacing = s;
+  }
+
+  /** sets the color of the LEDs in RGB */
+  public void setColor(LightCodes light) {
+    int r = 0;
+    int g = 0;
+    int b = 0;
+
+    if (light == LightCodes.CONE || light == LightCodes.CUBE || light == LightCodes.WORKING) {
+      r = 255;
+    }
+    if (light == LightCodes.CONE || light == LightCodes.GAME_OBJECT || light == LightCodes.NO_TAG) {
+      g = 255;
+    }
+    if (light == LightCodes.CUBE || light == LightCodes.READY_TO_SCORE) {
+      b = 255;
+    }
+    if (light == LightCodes.OFF) {
+      r = 0;
+      g = 0;
+      b = 0;
+    }
+
+    for (int i = 0; i < m_ledBuffer.getLength(); i = i + spacing) {
+      m_ledBuffer.setRGB(i, r, g, b);
     }
   }
 
