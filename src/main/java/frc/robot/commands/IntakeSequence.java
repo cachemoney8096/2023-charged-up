4613db7476f9911effea8119badb4fe5981b69b0
tails618 <asher.grossman@gmail.com>
2023-02-08 15:49:12 -0600

    Added comments

--- src/main/java/frc/robot/commands/IntakeSequence.java
+++ src/main/java/frc/robot/commands/IntakeSequence.java
@@ -18,21 +18,31 @@ public class IntakeSequence extends SequentialCommandGroup {
   private ParallelRaceGroup deployIntake =
       new RunCommand(intake::deploy, intake).withTimeout(Cal.Intake.AUTO_CLAMP_WAIT_TIME_SECONDS);
 
-  // run intake, move lift to intake position, and open the grabber, until the robot sees an object
-  // and the lift is in position
+  // run intake infinitely (until parallel command ends)
   private RunCommand runIntake = new RunCommand(intake::intakeGamePiece, intake);
+  
+  // trigger the lift to move to the intake position. This does not need a timeout because it is
+  // running in the parallel group, which is controlled by lift.readyToIntake()
   private InstantCommand liftToIntakePos =
       new InstantCommand(() -> lift.setDesiredPosition(LiftPosition.GRAB_FROM_INTAKE), lift);
+
+  // triggers the grabber to open when it is safe. Again, we do not care about time because it is in
+  // the parallel group.
   private InstantCommand openGrabber = new InstantCommand(lift::openGrabber, lift);
+
+  // run intake, move lift to intake position, and open the grabber, until the robot sees an object
+  // and the lift is in position
   private ParallelRaceGroup intakePrep =
       new ParallelCommandGroup(runIntake, liftToIntakePos, openGrabber).until(lift::readyToIntake);
 
+  // triggers the grabber to close
   private InstantCommand closeGrabber = new InstantCommand(lift::closeGrabber, lift);
-  
+
+  // immediately unclamps the intake.
   private InstantCommand unclampIntake = new InstantCommand(intake::unclampIntake, intake);
 
-  // this does not need a timeout even though it is a longer action, because it is the final action
-  // in the sequence
+  // triggers the lift to move to the starting position. This does not need a timeout even though it
+  // is a longer action, because it is the final action in the sequence
   private InstantCommand liftToStartPos =
       new InstantCommand(() -> lift.setDesiredPosition(LiftPosition.STARTING), lift);
 
