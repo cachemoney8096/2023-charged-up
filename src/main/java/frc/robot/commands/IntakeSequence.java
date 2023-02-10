bf7b34c3d544a5e00fc421d478d7d83e43d67ed6
tails618 <asher.grossman@gmail.com>
2023-02-10 13:52:38 -0600

    Moved grabber outside of parallel group so scheduling works correctly

--- src/main/java/frc/robot/commands/IntakeSequence.java
+++ src/main/java/frc/robot/commands/IntakeSequence.java
@@ -12,35 +12,27 @@ import frc.robot.subsystems.Lift;
 import frc.robot.subsystems.Lift.LiftPosition;
 
 public class IntakeSequence extends SequentialCommandGroup {
-  private Intake intake;
-  private Lift lift;
 
   public IntakeSequence(Intake intake, Lift lift) {
-    this.intake = intake;
-    this.lift = lift;
     addRequirements(intake, lift);
 
     addCommands(
         // deploy intake for specified amount of time
         new RunCommand(intake::deploy, intake).until(intake::atDesiredPosition),
 
-        // run intake, move lift to intake position, and open the grabber, until the robot sees an
-        // object
-        // and the lift is in position
+        // run intake and move lift to intake position, until the robot sees an
+        // object and the lift is in position
         new ParallelCommandGroup(
 
             // run intake
             new InstantCommand(intake::intakeGamePiece, intake),
 
-            // trigger the lift to move to the intake position. This does not need a timeout
-            // because it is
-            // running in the parallel group, which is controlled by readyToIntake()
-            new InstantCommand(() -> lift.setDesiredPosition(LiftPosition.GRAB_FROM_INTAKE), lift),
+            // trigger the lift to move to the intake position. This does not need a timeout because
+            // it is running in the parallel group, which is controlled by readyToIntake()
+            new InstantCommand(() -> lift.setDesiredPosition(LiftPosition.GRAB_FROM_INTAKE), lift)),
 
-            // triggers the grabber to open when it is safe. Again, we do not care about time
-            // because it is in
-            // the parallel group.
-            new InstantCommand(lift::openGrabber, lift)),
+        // triggers the grabber to open when it is safe
+        new InstantCommand(lift::openGrabber, lift),
 
         // wait until the lift is in position and the intake sees a game piece
         new WaitUntilCommand(
@@ -63,8 +55,7 @@ public class IntakeSequence extends SequentialCommandGroup {
         new WaitCommand(Cal.Intake.UNCLAMP_TIME_SECONDS),
 
         // triggers the lift to move to the starting position. This does not need a timeout even
-        // though it
-        // is a longer action, because it is the final action in the sequence
+        // though it is a longer action, because it is the final action in the sequence
         new InstantCommand(() -> lift.setDesiredPosition(LiftPosition.STARTING), lift));
   }
 }
