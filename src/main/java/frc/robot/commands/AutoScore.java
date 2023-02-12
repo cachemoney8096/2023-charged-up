7bad3a0270878dcfb7eac39f42f0a777ec88b840
Pxndah <gujjasidharth@gmail.com>
2023-02-11 18:07:46 -0600

    made changes to AutoScore
    * added requirements for AutoScore
    * removed `private DriveSubsystem drive` as it was unncessary
    * changed isFirstPath from true to false
    * added respective comments

--- src/main/java/frc/robot/commands/AutoScore.java
+++ src/main/java/frc/robot/commands/AutoScore.java
@@ -12,26 +12,34 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
-
 import frc.robot.Cal;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 public class AutoScore extends SequentialCommandGroup{
-    private DriveSubsystem drive;
     private PathPlannerTrajectory trajInit =
         PathPlanner.loadPath(
     "InitScoreAndGetGamePiece",
         new PathConstraints(Cal.PLACEHOLDER_DOUBLE, Cal.PLACEHOLDER_DOUBLE));
+
     private PathPlannerTrajectory trajCharge =
         PathPlanner.loadPath(
     "ScoringLocToChargeStation",
         new PathConstraints(Cal.PLACEHOLDER_DOUBLE, Cal.PLACEHOLDER_DOUBLE));
+
     private HashMap<String, Command> eventMap = new HashMap<>();
-    private boolean isFirstPath = true;
+    private boolean isFirstPath = false;
     
+    /** Events include: open intake and close intake before and after obtaining game piece */
     public AutoScore(Lift lift, Intake intake, DriveSubsystem drive, TagLimelight tagLimelight){
+        addRequirements(lift, intake, drive, tagLimelight)
+         /** Events include: open intake and close intake before and after obtaining game piece, */
         eventMap.put("deployIntake", new InstantCommand(()->{intake.setDesiredDeployed(true);}, intake));
         eventMap.put("closeIntake", new InstantCommand(()->{intake.setDesiredDeployed(false);}, intake));
+        /**TODO: configure limelight to "take over" driving process after certain point 
+        AFTER obtaining game piece to be more accurate with distance */
+
+
+        /** Initialize sequential commands that run for the "15 second autonomous phase" */
         addCommands(
             new InstantCommand(lift::manualPrepScore, lift),
             new WaitUntilCommand(()->lift.atPosition(LiftPosition.MANUAL_PREP_SCORE)),
@@ -42,7 +50,9 @@ public class AutoScore extends SequentialCommandGroup{
             new FollowPathWithEvents(
                 drive.followTrajectoryCommand(trajInit, true),
                 trajInit.getMarkers(), eventMap),
-            /*limelight goes here */
+
+            /** TODO: Limelight code goes here */
+
             new InstantCommand(lift::manualPrepScore, lift),
             new WaitUntilCommand(()->lift.atPosition(LiftPosition.MANUAL_PREP_SCORE)),
             drive.followTrajectoryCommand(trajCharge, isFirstPath), //this does not accept the FollowPathWithEvents
