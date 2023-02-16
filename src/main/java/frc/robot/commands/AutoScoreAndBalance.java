b182db68fd81670db813850cdfe793aa931a3810
voidSevenSevenSix <86328356+voidSevenSevenSix@users.noreply.github.com>
2023-02-15 20:36:48 -0600

    Rewrote AutoScoreAndBalance

--- src/main/java/frc/robot/commands/AutoScoreAndBalance.java
+++ src/main/java/frc/robot/commands/AutoScoreAndBalance.java
@@ -3,10 +3,14 @@ package frc.robot.commands;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Cal;
 import frc.robot.subsystems.Lift;
+import frc.robot.subsystems.Lift.LiftPosition;
 import frc.robot.subsystems.drive.DriveSubsystem;
 
 /** Scores a game piece, then drives onto the charge station and balances */
@@ -19,11 +23,14 @@ public class AutoScoreAndBalance extends SequentialCommandGroup {
               Cal.SwerveSubsystem.MAX_LINEAR_ACCELERATION_METERS_PER_SEC_SQ));
 
   public AutoScoreAndBalance(boolean isFirstPath, Lift lift, DriveSubsystem drive) {
-    // TODO update this since scoreGamePiece and doneScoring are stubbed
     addCommands(
-        new RunCommand(lift::scoreGamePiece, lift).until(lift::doneScoring),
-        drive.followTrajectoryCommand(traj, isFirstPath),
-        new AutoChargeStationBalance(drive));
+      new InstantCommand(lift::ManualPrepScoreSequence, lift),
+      new WaitUntilCommand(() -> lift.atPosition(LiftPosition.PRE_SCORE_HIGH_CONE)),
+      new InstantCommand(lift::startScore, lift),
+      new WaitUntilCommand(() -> lift.atPosition(LiftPosition.SCORE_HIGH_CONE)),
+      new finishScore(lift),
+      drive.followTrajectoryCommand(traj, isFirstPath),
+      new AutoChargeStationBalance(drive)); 
   }
 
   public PathPlannerTrajectory getTrajectory() {
