c9927e1a8148bd8f84535a235f13713c78caace5
avakimc <acohen1@ucls.uchicago.edu>
2023-02-22 19:59:20 -0600

    Revised Lights Controls to Use Blinkin
    - removed rainbow code and replaced it with the built-in blinkin rainbow code
    - unsure of how to change spacing when using blinkin, if even necessary

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -1,17 +1,15 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.RobotMap;
 import java.util.TreeMap;
 
 /** Controls the LEDs that indicate robot status */
 public class Lights extends SubsystemBase {
   private LightCode currentLightStatus = LightCode.OFF;
-  private TreeMap<LightCode, AddressableLEDBuffer> lightOptionsMap;
-  private AddressableLED led = new AddressableLED(RobotMap.LED_PWM_PORT);
+  private TreeMap<LightCode, Double> lightOptionsMap;
+  private Spark m_blinkin = new Spark(RobotMap.LED_PWM_PORT);
 
   /** for example, a spacing value of 1 would mean every LED is on */
   private int spacing = 1;
@@ -25,10 +23,6 @@ public class Lights extends SubsystemBase {
 
   /** true if party mode is currently on */
   private boolean partyMode = false;
-  /** indicates the current color the light is on while in party mode
-   * colors are in order of the rainbow (ex. 0 = red (WORKING))
-   */
-  private int colorIndex = 0;
 
   public enum LightCode {
     CONE, // Solid Yellow
@@ -42,32 +36,14 @@ public class Lights extends SubsystemBase {
   }
 
   public Lights() {
-    lightOptionsMap = new TreeMap<LightCode, AddressableLEDBuffer>();
-    lightOptionsMap.put(LightCode.CONE, new AddressableLEDBuffer(Constants.LED_LENGTH));
-    lightOptionsMap.put(LightCode.CUBE, new AddressableLEDBuffer(Constants.LED_LENGTH));
-    lightOptionsMap.put(LightCode.GAME_OBJECT, new AddressableLEDBuffer(Constants.LED_LENGTH));
-    lightOptionsMap.put(LightCode.NO_TAG, new AddressableLEDBuffer(Constants.LED_LENGTH));
-    lightOptionsMap.put(LightCode.WORKING, new AddressableLEDBuffer(Constants.LED_LENGTH));
-    lightOptionsMap.put(LightCode.READY_TO_SCORE, new AddressableLEDBuffer(Constants.LED_LENGTH));
-    lightOptionsMap.put(LightCode.ORANGE, new AddressableLEDBuffer(Constants.LED_LENGTH));
-    lightOptionsMap.put(LightCode.OFF, new AddressableLEDBuffer(Constants.LED_LENGTH));
-
-    led.setLength(lightOptionsMap.get(LightCode.OFF).getLength());
-
-    setUpBuffer(lightOptionsMap.get(LightCode.CONE), 255, 255, 0);
-    setUpBuffer(lightOptionsMap.get(LightCode.CUBE), 255, 0, 255);
-    setUpBuffer(lightOptionsMap.get(LightCode.GAME_OBJECT), 0, 255, 0);
-    setUpBuffer(lightOptionsMap.get(LightCode.NO_TAG), 0, 255, 0);
-    setUpBuffer(lightOptionsMap.get(LightCode.WORKING), 255, 0, 0);
-    setUpBuffer(lightOptionsMap.get(LightCode.READY_TO_SCORE), 0, 0, 255);
-    setUpBuffer(lightOptionsMap.get(LightCode.ORANGE), 255, 150, 0);
-    setUpBuffer(lightOptionsMap.get(LightCode.OFF), 0, 0, 0);
-  }
-
-  private void setUpBuffer(AddressableLEDBuffer buffer, int r, int g, int b) {
-    for (var i = 0; i < buffer.getLength(); i += spacing) {
-      buffer.setRGB(i, 255, 0, 0);
-    }
+    lightOptionsMap = new TreeMap<LightCode, Double>();
+    lightOptionsMap.put(LightCode.CONE, 0.69);
+    lightOptionsMap.put(LightCode.CUBE, 0.91);
+    lightOptionsMap.put(LightCode.GAME_OBJECT, 0.71);
+    lightOptionsMap.put(LightCode.NO_TAG, 0.71);
+    lightOptionsMap.put(LightCode.WORKING, 0.61);
+    lightOptionsMap.put(LightCode.READY_TO_SCORE, 0.87);
+    lightOptionsMap.put(LightCode.OFF, 0.99); // 0.99 = black, is this equivalent to OFF?
   }
 
   public void toggleCode(LightCode light) {
@@ -76,14 +52,12 @@ public class Lights extends SubsystemBase {
     } else {
       currentLightStatus = light;
     }
-    led.setData(lightOptionsMap.get(currentLightStatus));
-    led.start();
+    m_blinkin.set(lightOptionsMap.get(currentLightStatus));
   }
 
   public void setLight(LightCode light) {
     currentLightStatus = light;
-    led.setData(lightOptionsMap.get(currentLightStatus));
-    led.start();
+    m_blinkin.set(lightOptionsMap.get(currentLightStatus));
   }
 
   /** Sets the spacing between each LED so not all are lit up when applicable */
@@ -102,50 +76,20 @@ public class Lights extends SubsystemBase {
     if (currentLightStatus == LightCode.NO_TAG) {
       if (blinkingTimer >= blinkingPeriod) {
         if (blinkingColorOn) {
-          led.setData(lightOptionsMap.get(LightCode.OFF));
+          m_blinkin.set(lightOptionsMap.get(LightCode.OFF));
         } else {
-          led.setData(lightOptionsMap.get(LightCode.NO_TAG));
+          m_blinkin.set(lightOptionsMap.get(LightCode.NO_TAG));
         }
         blinkingColorOn = !blinkingColorOn;
       }
       blinkingTimer++;
+    } else {
+      blinkingTimer = 0;
     }
 
     /* if party mode should be running, then start a party */
-    else if (partyMode) {
-      if (blinkingTimer >= blinkingPeriod) {
-        switch (colorIndex) {
-          case 0:
-            led.setData(lightOptionsMap.get(LightCode.WORKING));
-            break;
-          case 1:
-            led.setData(lightOptionsMap.get(LightCode.ORANGE));
-            break;
-          case 2:
-            led.setData(lightOptionsMap.get(LightCode.CONE));
-            break;
-          case 3:
-            led.setData(lightOptionsMap.get(LightCode.GAME_OBJECT));
-            break;
-          case 4:
-            led.setData(lightOptionsMap.get(LightCode.READY_TO_SCORE));
-            break;
-          case 5:
-            led.setData(lightOptionsMap.get(LightCode.CUBE));
-            break;
-          default:
-            // case should never be reached
-            led.setData(lightOptionsMap.get(LightCode.OFF));
-        }
-        if (colorIndex == 5) {
-          colorIndex = 0;
-        } else {
-          colorIndex++;
-        }
-        blinkingTimer++;
-      }
-    } else {
-      blinkingTimer = 0;
+    if (partyMode) {
+      m_blinkin.set(-0.97);
     }
   }
 }
