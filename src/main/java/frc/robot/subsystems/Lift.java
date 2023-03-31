3b4852e77c29d0aa86aab9f3bd38198f1a8c20c8
Shared ThinkPad <cachemoney8096@gmail.com>
2023-03-31 14:42:25 -0500

    before qual 46

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -20,6 +20,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -164,10 +165,10 @@ public class Lift extends SubsystemBase {
         new Pair<Double, Double>(Cal.Lift.ELEVATOR_HIGH_POSITION_INCHES, 213.0));
     liftPositionMap.put(
         LiftPosition.PRE_SCORE_MID_CONE,
-        new Pair<Double, Double>(Cal.Lift.ELEVATOR_LOW_POSITION_INCHES, 196.0));
+        new Pair<Double, Double>(Cal.Lift.ELEVATOR_LOW_POSITION_INCHES, 193.0));
     liftPositionMap.put(
         LiftPosition.PRE_SCORE_HIGH_CONE,
-        new Pair<Double, Double>(Cal.Lift.ELEVATOR_HIGH_POSITION_INCHES, 196.0));
+        new Pair<Double, Double>(Cal.Lift.ELEVATOR_HIGH_POSITION_INCHES, 193.0));
     liftPositionMap.put(
         LiftPosition.POST_SCORE_HIGH,
         new Pair<Double, Double>(Cal.Lift.ELEVATOR_HIGH_POSITION_INCHES, 180.0));
@@ -307,10 +308,14 @@ public class Lift extends SubsystemBase {
         Cal.Lift.ELEVATOR_FEEDFORWARD.calculate(elevatorController.getSetpoint().velocity);
     elevatorLeft.setVoltage(elevatorDemandVolts);
 
-    double armDemandVolts = armController.calculate(armEncoder.getPosition());
-    armDemandVolts += Cal.Lift.ARM_FEEDFORWARD.calculate(armController.getSetpoint().velocity);
-    armDemandVolts += Cal.Lift.ARBITRARY_ARM_FEED_FORWARD_VOLTS * getCosineArmAngle();
-    armMotor.setVoltage(armDemandVolts);
+    double armDemandVoltsA = armController.calculate(armEncoder.getPosition());
+    double armDemandVoltsB = Cal.Lift.ARM_FEEDFORWARD.calculate(armController.getSetpoint().velocity);
+    double armDemandVoltsC = Cal.Lift.ARBITRARY_ARM_FEED_FORWARD_VOLTS * getCosineArmAngle();
+    armMotor.setVoltage(armDemandVoltsA + armDemandVoltsB + armDemandVoltsC);
+
+    SmartDashboard.putNumber("Arm PID", armDemandVoltsA);
+    SmartDashboard.putNumber("Arm FF", armDemandVoltsB);
+    SmartDashboard.putNumber("Arm Gravity", armDemandVoltsC);
   }
 
   public void closeGrabber() {
