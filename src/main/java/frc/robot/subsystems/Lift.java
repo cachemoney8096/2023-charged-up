fbccf6ac875c5ebb3ae0b0705830560f2ef63420
Asher Grossman <asher.grossman@gmail.com>
2023-07-05 23:25:38 -0500

    Replaces BOOT_UP with ALT_HOME, adjusts shelf position, adjusts prints

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -104,9 +104,9 @@ public class Lift extends SubsystemBase {
   private final DigitalInput gamePieceSensor = new DigitalInput(RobotMap.LIFT_GAME_PIECE_DIO);
 
   // Members
-  private LiftPosition latestPosition = LiftPosition.BOOT_UP;
-  private LiftPosition desiredPosition = LiftPosition.BOOT_UP;
-  private LiftPosition goalPosition = LiftPosition.BOOT_UP;
+  private LiftPosition latestPosition = LiftPosition.ALT_HOME;
+  private LiftPosition desiredPosition = LiftPosition.ALT_HOME;
+  private LiftPosition goalPosition = LiftPosition.ALT_HOME;
   private boolean desiredGrabberClosed = true;
   public ScoringLocationUtil scoreLoc;
   private boolean scoringInProgress = false;
@@ -148,7 +148,7 @@ public class Lift extends SubsystemBase {
         LiftPosition.GRAB_FROM_INTAKE,
         new Pair<Double, Double>(Cal.Lift.ELEVATOR_LOW_POSITION_INCHES, 84.0));
     liftPositionMap.put(
-        LiftPosition.SHELF, new Pair<Double, Double>(Cal.Lift.ELEVATOR_LOW_POSITION_INCHES, 188.0));
+        LiftPosition.SHELF, new Pair<Double, Double>(Cal.Lift.ELEVATOR_LOW_POSITION_INCHES, 190.0));
     liftPositionMap.put(
         LiftPosition.SCORE_LOW,
         new Pair<Double, Double>(Cal.Lift.ELEVATOR_LOW_POSITION_INCHES, 187.0));
@@ -272,7 +272,7 @@ public class Lift extends SubsystemBase {
         new Pair<Double, Double>(curPos.getFirst(), curPos.getSecond() - 0.5);
     liftPositionMap.replace(desiredPosition, newPos);
 
-    new PrintCommand("Latest angle for " + desiredPosition + ": " + (curPos.getSecond() - 0.5));
+    System.out.println("Latest angle for " + desiredPosition + ": " + (curPos.getSecond() - 0.5));
 
     armController.setGoal(newPos.getSecond());
   }
@@ -283,7 +283,7 @@ public class Lift extends SubsystemBase {
         new Pair<Double, Double>(curPos.getFirst(), curPos.getSecond() + 0.5);
     liftPositionMap.replace(desiredPosition, newPos);
 
-    new PrintCommand("Latest angle for " + desiredPosition + ": " + (curPos.getSecond() + 0.5));
+    System.out.println("Latest angle for " + desiredPosition + ": " + (curPos.getSecond() + 0.5));
 
     armController.setGoal(newPos.getSecond());
   }
